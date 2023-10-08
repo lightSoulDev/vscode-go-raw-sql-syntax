a5ab4c0d36024d3026de96fc3e59a9dcab24c01a
Nurzhan Kozhanov <nurzhan.k@lightsoul.dev>
2023-10-09 00:41:37 +0600

    remove redundant ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,6 @@
 import * as vscode from "vscode";
 
-export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "go-raw-sql-syntax" is now active!'
-  );
-
-  // let disposable = vscode.languages.registerDocumentSymbolProvider("go", {
-  //   provideDocumentSymbols: (document, token) => {
-  //     // Add your symbol detection logic here if needed
-  //     return [];
-  //   },
-  // });
-
-  // context.subscriptions.push(disposable);
-}
+export function activate(context: vscode.ExtensionContext) {}
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
